3773bddad84678358faf15a503430b1179f60a38
Dominic Burkart <1351120+DominicBurkart@users.noreply.github.com>
2019-12-06 10:06:10 +0100

    add pwa basics

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,32 @@
 const path = require('path');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {GenerateSW} = require('workbox-webpack-plugin');
+
 
 const distPath = path.resolve(__dirname, "dist");
+const assetPath = path.join(__dirname, "assets");
 module.exports = (env, argv) => {
   return {
     devServer: {
-      contentBase: distPath,
-      compress: argv.mode === 'production',
+      contentBase: [distPath, assetPath],
+      compress: argv.mode === "production",
       port: 8000
     },
     entry: './bootstrap.js',
     output: {
       path: distPath,
       filename: "todomvc.js",
-      webassemblyModuleFilename: "todomvc.wasm"
+      webassemblyModuleFilename: "todomvc.wasm",
     },
+    module: {
+     rules: [
+       {
+        test: /\.(png|svg|jpg|gif)$/,
+         use: ['file-loader']
+       },
+     ],
+   },
     plugins: [
       new CopyWebpackPlugin([
         { from: './static', to: distPath }
@@ -23,7 +34,11 @@ module.exports = (env, argv) => {
       new WasmPackPlugin({
         crateDirectory: ".",
         extraArgs: "--no-typescript",
-      })
+      }),
+      new GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+     }),
     ],
     watch: argv.mode !== 'production'
   };
